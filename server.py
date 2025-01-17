abb934f073d130a15d8e7bd6b6b37f6a89cb5e2f
Robert Yoka <ryoka@ycp.edu>
2025-01-16 19:36:02 -0500

    complete project

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-# Import the Maths package here
+from Maths.mathematics import summation, subtraction, multiplication
 
 app = Flask("Mathematics Problem Solver")
 
@@ -7,23 +7,23 @@ app = Flask("Mathematics Problem Solver")
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    return str(summation(num1, num2))
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    return str(subtraction(num1, num2))
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    return str(multiplication(num1, num2))
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
